a771ad3dee29d3d9d3fd8aa8e701e6790f37750a
Jeremy Jasa <jjjasa@my365.bellevue.edu>
2023-04-02 16:01:16 -0500

    Add files via upload

---
+++ week-3/Jasa-favorites.js
@@ -0,0 +1,27 @@
+/*
+*********************************************
+Title: favorites.js
+Author: Jeremy Jasa
+Date: 02 April 2023
+Description: The JavaScript for the favorites app.
+**********************************************
+*/
+document.getElementById("sport").onclick = function() 
+{
+alert("Carolina Panthers");
+};
+
+document.getElementById("book").onclick = function() 
+{
+alert("The Hitchhiker's Guide to the Galaxy");
+};
+
+document.getElementById("author").onclick = function() 
+{
+alert("Shel Silverstein");
+};
+
+document.getElementById("language").onclick = function() 
+{
+alert("C#");
+};
