a912f5142bea8365a92cd1c808d7e850676adff4
Jeremy Jasa <jjjasa@my365.bellevue.edu>
2023-04-02 22:58:42 -0500

    Add files via upload

---
+++ week-3/Jasa-temp-conversion.js
@@ -0,0 +1,15 @@
+/*
+*********************************************
+Title: temp-conversion.js
+Author: Jeremy Jasa
+Date: 02 April 2023
+Description: The JavaScript for the temp-conversion app.
+**********************************************
+*/
+document.getElementById("Temperature").onclick = function() 
+{
+let inputText = document.getElementById("input-int").value;
+document.getElementById("java-script-output").innerHTML = inputText;
+};
+
+
